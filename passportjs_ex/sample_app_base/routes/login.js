b2b7186a24286fe34ba4efe7bd4ca43add214100
C.Fulton <cifulton@uic.edu>
2021-10-29 14:51:52 -0500

    adding changes to login.hbs

--- passportjs_ex/sample_app_base/routes/login.js
+++ passportjs_ex/sample_app_base/routes/login.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('login');
-});
+    // Make a request to the API 
+        axios.get('https://api.chucknorris.io/jokes/random')
+        .then(function (response) {
+        // handle success
+        let jokeData = response.data;
+        res.render('login', {data: jokeData});
+        })
+        .catch(function (error) {
+        // handle error
+        console.log(error);
+        })
+        .then(function () {
+        // always executed
+        });
+            
+        });
 
 module.exports = router;
